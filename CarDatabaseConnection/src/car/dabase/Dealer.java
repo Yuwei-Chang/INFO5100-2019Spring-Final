21ac7cd25f11a9928d37afab26969620c809671f
rekhasaribala10 <46608046+rekhasaribala10@users.noreply.github.com>
2019-04-12 12:49:23 -0700

    Add files via upload

---
+++ CarDatabaseConnection/src/car/dabase/Dealer.java
@@ -0,0 +1,16 @@
+package car.dabase;
+
+public class Dealer {
+
+	private String dealerId;
+	
+	private String dealerName;
+	
+	private String dealerAddress;
+	
+	private String zipCode;
+	
+	private String phoneNumber;
+	
+	
+}
---
+++ CarDatabaseConnection/src/car/dabase/MainClass.java
@@ -0,0 +1,15 @@
+package car.dabase;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		DatabaseConnection connection = new DatabaseConnection();
+		Vehicle vehicle = connection.retriveVehicleFromDatabase("V1");
+		
+		System.out.println(vehicle.getVehicleId() + " " + vehicle.getYear() + " " + vehicle.getPrice());
+		
+		
+		
+	}
+
+}
