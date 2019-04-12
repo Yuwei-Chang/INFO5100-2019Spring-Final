621d20cc757d84d7bb79d228a60614218fe7de0a
Ashwini292 <46771557+Ashwini292@users.noreply.github.com>
2019-04-12 16:29:40 -0700

    Update DealerDB.java

--- src/database/DealerDB.java
+++ src/database/DealerDB.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 
 public class DealerDB extends Dealer{
 	String URL = "jdbc:sqlserver://is-swang01.ischool.uw.edu;"
-			+ "databaseName=Dealer";
+			+ "databaseName=Car_Inventory";
 	String USER = "INFO6210";
 	String PASS = "NEUHusky!";
 
