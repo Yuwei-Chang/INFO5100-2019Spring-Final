39fe9f724db9c7b3cc82f0c7bbb54902cf0d8938
jenniferyuexiao <46546080+jenniferyuexiao@users.noreply.github.com>
2019-04-19 13:07:37 -0700

    Update and rename SortType.java to SortLogic.java

--- src/searchDealerLogic/SortLogic.java
+++ src/searchDealerLogic/SortLogic.java
@@ -1,5 +1,7 @@
 package searchDealerLogic;
 
-public enum SortType{
+public interface SortLogic{
+  public enum SortType implements SortLogic {
     PRICE_ASC, PRICE_DSC, YEAR_ASC, YEAR_DSC, MILEAGE_ASC, MILEAGE_DSC
+  }
 }
