ed15498d7714cf049da7afe8b6571abd2ff98f7a
xinyuezhang93 <zhang.xinyue@husky.neu.edu>
2019-04-18 23:49:54 -0700

    search inventory

---
+++ src/InventorySearch/InventorySearcher.java
@@ -0,0 +1,22 @@
+package InventorySearcher;
+
+import dto.Vehicle;
+import java.util.ArrayList;
+
+public interface InventorySearcher {
+	ArrayList<Vehicle> search_inventory(ArrayList<Vehicle> inventory,
+													  String _new,
+													  String _used,
+													  String min_year,
+												      String max_year,
+													  String max_mileage,
+													  String min_price,
+													  String max_price,
+													  String model,
+													  String make,
+													  String type,
+													  String seat_count);
+}
+
+// use InventorySearcher.search_inventory(...) to get search result: return type: ArrayList<Vehicle>
+// put default value as null or "" if filters not be filled.
