77ca5635552249630873db6b84ca0296e5aa35aa
Amisha <44983805+AmishaJain10@users.noreply.github.com>
2019-04-26 10:29:04 -0700

    updated code

--- src/database/VehiclesSearchResult.java
+++ src/database/VehiclesSearchResult.java
@@ -1,4 +1,4 @@
-package DB;
+package database;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -11,54 +11,61 @@ import java.util.List;
 import java.util.Properties;
 
 public class VehiclesSearchResult {
+
+    DatabaseConnection dbObj =new DatabaseConnection();
+
     String URL = "jdbc:sqlserver://is-swang01.ischool.uw.edu;"
             + "databaseName=Car_Inventory";
     String USER = "";
     String PASS = "";
 
     public List getMake() {
-        List makeList=new ArrayList<String>();
-        try {
-            InputStream input = new FileInputStream("DB.properties");
-            Properties prop=new Properties();
-            prop.load(input);
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection conn = DriverManager.getConnection(URL, prop.getProperty("USER"), prop.getProperty("PASS"));
+//        List makeList=new ArrayList<String>();
+//        try {
+//            InputStream input = new FileInputStream("src/database/connection.properties");
+//            Properties prop=new Properties();
+//            prop.load(input);
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            Connection conn = DriverManager.getConnection(URL, prop.getProperty("username"), prop.getProperty("password"));
+//
+//            PreparedStatement statement =conn.prepareStatement("SELECT DISTINCT Make from  dbo.vehicle" );
+//            ResultSet rs = statement.executeQuery();
+//
+//            while (rs.next()) {
+//                makeList.add(rs.getString(1));
+//            }
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return makeList;
 
-            PreparedStatement statement =conn.prepareStatement("SELECT DISTINCT Make from  dbo.vehicle" );
-            ResultSet rs = statement.executeQuery();
-
-            while (rs.next()) {
-                makeList.add(rs.getString(1));
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return makeList;
+        return  dbObj.getMake1();
     }
 
     public List getModel() {
-        List modelList=new ArrayList<String>();
+//        List modelList=new ArrayList<String>();
+//
+//        // GetMakeandModel makeandModel = new GetMakeandModel();
+//        try {
+//            InputStream input = new FileInputStream("src/database/connection.properties");
+//            Properties prop=new Properties();
+//            prop.load(input);
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            Connection conn = DriverManager.getConnection(URL, prop.getProperty("username"), prop.getProperty("password"));
+//
+//            PreparedStatement statement =conn.prepareStatement("SELECT DISTINCT Model from  dbo.vehicle" );
+//            ResultSet rs = statement.executeQuery();
+//
+//            while(rs.next()) {
+//                modelList.add(rs.getString(1));
+//            }
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return modelList;
 
-        // GetMakeandModel makeandModel = new GetMakeandModel();
-        try {
-            InputStream input = new FileInputStream("DB.properties");
-            Properties prop=new Properties();
-            prop.load(input);
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection conn = DriverManager.getConnection(URL, prop.getProperty("USER"), prop.getProperty("PASS"));
-
-            PreparedStatement statement =conn.prepareStatement("SELECT DISTINCT Model from  dbo.vehicle" );
-            ResultSet rs = statement.executeQuery();
-
-            while(rs.next()) {
-                modelList.add(rs.getString(1));
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return modelList;
-    }
+   return dbObj.getModel1();
+   }
 }
